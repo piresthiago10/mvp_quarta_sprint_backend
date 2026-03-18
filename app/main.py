907d91533799782f00b23b5b7deb43482d187c34
Thiago Pires <thiagopires.as@gmail.com>
2026-03-17 21:15:53 -0300

    add tags

--- app/main.py
+++ app/main.py
@@ -8,3 +8,20 @@ from model import *
 # from logger import logger
 from schemas import *
 from flask_cors import CORS
+
+# Instanciando o objeto OpenAPI
+info = Info(title="IndividuoGenZ", version="1.0.0")
+app = OpenAPI(
+    __name__, info=info, static_folder="../front", static_url_path="/front"
+)
+CORS(app)
+
+# Definindo tags para agrupamento das rotas
+home_tag = Tag(
+    name="Documentação",
+    description="Seleção de documentação: Swagger, Redoc ou RapiDoc",
+)
+individuo_tag = Tag(
+    name="IndividuoGenZ",
+    description="Adição, visualização, remoção e predição de pacientes com Diabetes",
+)
